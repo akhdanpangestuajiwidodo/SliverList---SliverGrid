db880cf057fc9a5585523f43393ace042cbeedde
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-08 08:15:53 +0700

    implement SliverAppBarDelegate to delegate (error fix)

--- lib/screens/LearningPathList.dart
+++ lib/screens/LearningPathList.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sliverlist_and_slivergrid/screens/SliverAppBarDelegate.dart';
 
 class LearningPathList extends StatelessWidget {
   SliverPersistentHeader _header(String text) {
     return SliverPersistentHeader(
-      delegate: null,
+      delegate: SliverAppBarDelegate(
+        minHeight: 60,
+        maxHeight: 150,
+        child: Container(
+          color: Colors.lightBlue,
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
       pinned: true,
     );
   }
