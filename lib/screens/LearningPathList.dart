8fed1672e7416d69de10b232de08b37be1eb40e5
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-08 08:19:22 +0700

    implement SliverList and create _buildTile widget

--- lib/screens/LearningPathList.dart
+++ lib/screens/LearningPathList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sliverlist_and_slivergrid/model/academy.dart';
 import 'package:sliverlist_and_slivergrid/screens/SliverAppBarDelegate.dart';
 
 class LearningPathList extends StatelessWidget {
@@ -27,7 +28,17 @@ class LearningPathList extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         _header('Android Developer'),
+        SliverList(delegate: SliverChildListDelegate(
+          androidPaths.map(_buildTile).toList()
+        ))
       ],
+    );
+  }
+
+  Widget _buildTile(Academy academy) {
+    return ListTile(
+      title: Text(academy.title),
+      subtitle: Text(academy.description),
     );
   }
 }
